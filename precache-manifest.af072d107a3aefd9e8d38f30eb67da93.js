c274e88237753dbe9144616b5fac4c9ea2928738
Kai Warren <kaiwarren12@yahoo.com>
2021-02-26 15:59:24 -0800

    Updates

---
+++ precache-manifest.af072d107a3aefd9e8d38f30eb67da93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "559def704d7b281311ce",
+    "url": "/static/css/main.f0376554.chunk.css"
+  },
+  {
+    "revision": "559def704d7b281311ce",
+    "url": "/static/js/main.559def70.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1fee40fab8686aba5c79d032227e1b89",
+    "url": "/index.html"
+  }
+];
