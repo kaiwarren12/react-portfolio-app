6dd733ede151d65fb8f976a31ab7fbd98e2a11d1
Kai Warren <kaiwarren12@yahoo.com>
2021-02-26 15:46:40 -0800

    pushing portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,97 @@
+let resumeData = {
+  imagebaseurl: "https://github.com/kaiwarren12",
+  name: "Kai Warren",
+  role: "Full stack develop and Marketing Specialist",
+  linkedinId: "kaiwarren23@yahoo.com",
+  roleDescription:
+    " My name is Kai Warren, I'm a business graduate from Western Washington and aspiring full stack web developer! I have a passion for video games and grew up playing basketball.",
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/kai-warren-6bb159194/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/kaiwarren12",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    "My name is Kai Warren, I'm a business graduate from Western Washington and aspiring full stack web developer! I have a passion for video games and grew up playing basketball.",
+  address: "Washington",
+  website: "https://github.com/kaiwarren12",
+  education: [
+    {
+      UniversityName: "Western Washington University",
+      specialization: "Marketing",
+      MonthOfPassing: "Dec",
+      YearOfPassing: "2020",
+    },
+    {
+      UniversityName: "University of Washington",
+      specialization: "Full stack developer",
+      MonthOfPassing: "Febuary",
+      YearOfPassing: "2021",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Filter",
+      specialization: "Sales Development Rep",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2020",
+      Achievements:
+        "B2b email marketing, devloped key target account plans, etc",
+    },
+    {
+      CompanyName: "T-Mobile",
+      specialization: "Sales Associate",
+      YearOfLeaving: "2019",
+      Achievements: "Achieved #3 in all north west district sales",
+    },
+  ],
+  skillsDescription: "Your skills here",
+  skills: [
+    {
+      skillname: "HTML",
+    },
+    {
+      skillname: "CSS",
+    },
+    {
+      skillname: "Reactjs",
+    },
+    {
+      skillname: "MongoDB",
+    },
+    {
+      skillname: "JavaScript",
+    },
+    {
+      skillname: "Express",
+    },
+    {
+      skillname: "NodeJs",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "Password Generator",
+      imgurl: "images/screenShotProject1.png",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/screenShotProject2.png",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/screenShotProject3.png",
+    },
+  ],
+};
+
+export default resumeData;
